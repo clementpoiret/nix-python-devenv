9cb95f8400267ecff0aa5a7b8553487eb988bf98
clementpoiret <poiret.clement@outlook.fr>
2024-08-26 12:52:54 +0200

    feat: use uv instead of poetry

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Template of a Python project managed by poetry";
+  description = "Template of a Python project managed by uv";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
@@ -10,93 +10,75 @@
       url = "github:cachix/devenv";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    poetry2nix = {
-      url = "github:nix-community/poetry2nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs = inputs @ { flake-parts, ... }:
-    flake-parts.lib.mkFlake
-      {
-        inherit inputs;
-      }
-      {
-        imports = [
-          inputs.devenv.flakeModule
-        ];
-        flake = {
-          nixConfig = {
-            extra-substituters = [
-              "https://nixpkgs-python.cachix.org"
-              "https://cuda-maintainers.cachix.org"
-              "https://devenv.cachix.org"
-            ];
-            extra-trusted-public-keys = [
-              "nixpkgs-python.cachix.org-1:hxjI7pFxTyuTHn2NkvWCrAUcNZLNS3ZAvfYNuYifcEU="
-              "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
-              "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
-            ];
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ inputs.devenv.flakeModule ];
+      flake = {
+        nixConfig = {
+          extra-substituters = [
+            "https://nixpkgs-python.cachix.org"
+            "https://cuda-maintainers.cachix.org"
+            "https://devenv.cachix.org"
+          ];
+          extra-trusted-public-keys = [
+            "nixpkgs-python.cachix.org-1:hxjI7pFxTyuTHn2NkvWCrAUcNZLNS3ZAvfYNuYifcEU="
+            "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+            "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+          ];
+        };
+      };
+      systems = [ "x86_64-linux" ];
+      perSystem = { config, self', inputs', pkgs, lib, system, ... }: {
+        _module.args.pkgs = import inputs.nixpkgs {
+          inherit system;
+          # overlays = [
+          #   inputs.poetry2nix.overlays.default
+          #   (final: prev: { inherit (inputs.nixpkgs-stable) skopeo; })
+          # ];
+          config = {
+            allowUnfree = true;
+            cudaSupport = true;
           };
         };
-        systems = [ "x86_64-linux" ];
-        perSystem =
-          { config
-          , self'
-          , inputs'
-          , pkgs
-          , lib
-          , system
-          , ...
-          }:
-          {
-            _module.args.pkgs = import inputs.nixpkgs {
-              inherit system;
-              overlays = [
-                inputs.poetry2nix.overlays.default
-                (final: prev: {
-                  inherit (inputs.nixpkgs-stable) skopeo;
-                })
-              ];
-              config = {
-                allowUnfree = true;
-                cudaSupport = true;
-              };
-            };
-            devenv.shells.default =
-              let
-                inherit (inputs.poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryEnv;
-                buildInputs = with pkgs;[
-                  cudaPackages.cudatoolkit
-                  cudaPackages.cudnn
-                  cudaPackages.cuda_cudart
-                  pythonManylinuxPackages.manylinux2014Package
-                  stdenv.cc.cc
-                  zlib
-                ];
-              in
-              {
-                containers.default = {
-                  name = "python-poetry";
-                  startupCommand = "bash";
-                  copyToRoot = null;
-                };
-                env = {
-                  LD_LIBRARY_PATH = "${with pkgs; lib.makeLibraryPath buildInputs}:/run/opengl-driver/lib:/run/opengl-driver-32/lib";
-                  XLA_FLAGS = "--xla_gpu_cuda_data_dir=${pkgs.cudaPackages.cudatoolkit}"; # For tensorflow with GPU support
-                };
-                packages = with pkgs; [
-                  bashInteractive
-                ];
-                languages.python = {
-                  enable = true;
-                  manylinux.enable = false;
-                  package = pkgs.python312;
-                  poetry = {
-                    enable = true;
-                  };
-                };
-              };
+        devenv.shells.default = let
+          buildInputs = with pkgs; [
+            cudaPackages.cudatoolkit
+            cudaPackages.cudnn
+            cudaPackages.cuda_cudart
+            pythonManylinuxPackages.manylinux2014Package
+            stdenv.cc.cc
+            zlib
+          ];
+        in {
+          containers.default = {
+            name = "jimmy";
+            startupCommand = "bash";
+            copyToRoot = null;
           };
+          env = {
+            LD_LIBRARY_PATH = "${
+                with pkgs;
+                lib.makeLibraryPath buildInputs
+              }:/run/opengl-driver/lib:/run/opengl-driver-32/lib";
+            XLA_FLAGS =
+              "--xla_gpu_cuda_data_dir=${pkgs.cudaPackages.cudatoolkit}"; # For tensorflow with GPU support
+          };
+          packages = with pkgs; [ bashInteractive uv ];
+
+          enterShell = ''
+            uv sync --extra cuda12
+            . .venv/bin/activate
+          '';
+
+          languages.python = {
+            enable = true;
+            manylinux.enable = false;
+            package = pkgs.python312;
+            uv = { enable = true; };
+          };
+        };
       };
+    };
 }
